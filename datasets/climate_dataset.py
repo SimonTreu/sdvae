26b929a8a6e518af8850c22e8ab7516e173d9c7a
Simon Treu <sitreu@pik-potsdam.de>
2018-07-23 13:55:17 +0200

    implemented init method of datasets and added the necessary arg parameters

--- datasets/climate_dataset.py
+++ datasets/climate_dataset.py
@@ -1,12 +1,41 @@
 from torch.utils.data import Dataset
+import os.path
+
+TORCH_EXTENSION = [
+    '.pt'
+]
 
 
+# Dataset implementation
 class ClimateDataset(Dataset):
     def __init__(self, opt):
         self.opt = opt
+        self.root = opt.dataroot
+        self.dir_samples = os.path.join(opt.dataroot, opt.phase)
+        self.sample_paths = sorted(get_sample_files(self.dir_samples))
+        self.scale_factor = opt.scale_factor
+        # self.norm_parameters = opt.mean_std Todo implement getting the norm parameters while reading the arguments
 
     def __len__(self):
-        pass
+        return len(self.sample_paths)
 
     def __getitem__(self, item):
         pass
+
+
+# Helper Methods
+def is_torch_file(filename):
+    return any(filename.endswith(extension) for extension in TORCH_EXTENSION)
+
+
+def get_sample_files(sample_dir):
+    cells = []
+    assert os.path.isdir(sample_dir), '%s is not a valid directory' % sample_dir
+
+    for root, _, fnames in sorted(os.walk(sample_dir)):
+        for fname in fnames:
+            if is_torch_file(fname):
+                path = os.path.join(root, fname)
+                cells.append(path)
+
+    return cells
--- train.py
+++ train.py
@@ -2,8 +2,11 @@ from datasets.climate_dataset import ClimateDataset
 
 
 class Arg:
+    # Todo do that with an arg_parser
     def __init__(self):
-        pass
+        self.dataroot = "data/wind"
+        self.phase = "train"
+        self.scale_factor = 8
 
 
 args = Arg()
