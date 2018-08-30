70217fe3267f6032314365062edbbe13072d8e3f
Simon Treu <sitreu@pik-potsdam.de>
2018-08-30 11:57:35 +0200

    added Args Parser

---
+++ options/base_options.py
@@ -0,0 +1,88 @@
+import argparse
+import os.path
+import torch
+from utils import util
+from netCDF4 import Dataset
+
+
+class BaseOptions:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument('--name', type=str, default='vae_08_30')
+        self.parser.add_argument('--dataroot', type=str, default="data/wind",
+                                 help="path to images (should have subfolders trainA, trainB, valA, valB, etc)")
+        self.parser.add_argument('--phase', type=str, default="train", help="train, val, test, etc")
+        self.parser.add_argument('--fine_size', type=int, default=8,
+                                 help="number of high-resolution grid cells within one coarse resolution cell")
+        self.parser.add_argument('--batch_size', type=int, default=124, help='input batch size')
+        self.parser.add_argument('--no_shuffle', action='store_true',
+                                 help='if specified, do not shuffle the input data for batches')
+        self.parser.add_argument('--n_threads', type=int, default=4, help='# threads for loading data')
+        self.parser.add_argument('--nz', type=int, default=2, help='size of bottleneck')
+
+        self.parser.add_argument('--no', type=int, default=8,
+                                 help='size of encoded orography set 0 if non should be used')
+        self.parser.add_argument('--gpu_ids', type=str, default='-1',
+                                 help='gpu ids: e.g. 0  0,1,2, 0,2, -1 for CPU mode')
+        self.parser.add_argument('--n_epochs', type=int, default=20,
+                                 help='number of epochs (one epoch is training the hole data one time)')
+        self.parser.add_argument('--log_interval', type=int, default=100,
+                                 help='number of iterations until the next logging of cost values')
+        self.parser.add_argument('--plot_interval', type=int, default=1000000,
+                                 help='number of iterations until the next plotting of training results')
+        self.parser.add_argument('--lambda_cycle_l1', type=int, default=1000,
+                                 help='factor to be multiplied with the reconstruction loss '
+                                      '(|coarse(reconstructed fine resolution) - coarse input|)')
+        self.parser.add_argument('--lr', type=float, default=1e-3, help='learning rate for optimizer')
+        self.parser.add_argument('--save_interval', type=int, default=1, help='every _ epoch the model is saved')
+
+    def parse(self):
+        opt = self.parser.parse_args()
+
+        # plot selected options
+        args = vars(opt)
+        print('------------ Options -------------')
+        for k, v in sorted(args.items()):
+            if type(v) == bool and v:
+                if v:
+                    print(str(k))
+            else:
+                print('%s: %s' % (str(k), str(v)))
+        print('-------------- End ----------------')
+
+        # save to the disk
+        expr_dir = os.path.join('checkpoints', opt.name)
+        util.mkdirs(expr_dir)
+        file_name = os.path.join(expr_dir, 'opt.txt')
+        with open(file_name, 'wt') as opt_file:
+            opt_file.write('------------ Options -------------\n')
+            for k, v in sorted(args.items()):
+                if type(v) == bool:
+                    if v:
+                        opt_file.write(str(k))
+                else:
+                    opt_file.write('--%s %s\n' % (str(k), str(v)))
+            opt_file.write('-------------- End ----------------\n')
+
+        # parse gpu_ids
+        str_ids = opt.gpu_ids.split(',')
+        opt.gpu_ids = []
+        for str_id in str_ids:
+            int_id = int(str_id)
+            if int_id >= 0:
+                opt.gpu_ids_.append(int_id)
+        # set gpu ids
+        if len(opt.gpu_ids) > 0:
+            torch.cuda.set_device(opt.gpu_ids[0])
+
+        # load normalization values
+        with Dataset(os.path.join(opt.dataroot, "stats", "mean.nc4"), "r", format="NETCDF4") as rootgrp:
+            mean = float(rootgrp.variables['pr'][:])
+
+        with Dataset(os.path.join(opt.dataroot, "stats", "std.nc4"), "r", format="NETCDF4") as rootgrp:
+            std = float(rootgrp.variables['pr'][:])
+
+        opt.mean_std = {'mean': mean, 'std': std}
+        opt.threshold = (0 - opt.mean_std['mean']) / opt.mean_std['std']
+
+        return opt
--- train.py
+++ train.py
@@ -5,60 +5,28 @@ import torch
 import matplotlib.pyplot as plt
 from netCDF4 import Dataset
 import os
+from options.base_options import BaseOptions
 
-
-class Arg:
-    # Todo do that with an arg_parser
-    def __init__(self):
-        self.dataroot = "data/wind"
-        self.phase = "train"
-        self.fine_size = 8
-        self.batch_size = 124
-        self.no_shuffle = False
-        self.n_threads = 4
-        # Number of hidden variables
-        self.nz = 2
-        # size of encoded orography set 0 if non should be used
-        self.no = 8
-        self.gpu_ids = [-1]
-        self.n_epochs = 20
-        self.log_interval = 100
-        self.plot_interval = 1e6
-        self.lambda_cycle_l1 = 1000
-        # load normalization values
-        with Dataset(os.path.join(self.dataroot, "stats", "mean.nc4"), "r", format="NETCDF4") as rootgrp:
-            mean = float(rootgrp.variables['pr'][:])
-
-        with Dataset(os.path.join(self.dataroot, "stats", "std.nc4"), "r", format="NETCDF4") as rootgrp:
-            std = float(rootgrp.variables['pr'][:])
-
-        self.mean_std = {'mean': mean, 'std': std}
-        self.threshold = (0-self.mean_std['mean'])/self.mean_std['std']
-        self.name = 'vae_08_29_2'
-        self.lr = 1e-3
-        self.save_interval = 1
-
-
-args = Arg()
-device = torch.device("cuda" if args.gpu_ids[0] >= 0 else "cpu")
+opt = BaseOptions().parse()
+device = torch.device("cuda" if len(opt.gpu_ids) > 0 else "cpu")
 
 # create save dir
-save_root = os.path.join('checkpoints', args.name)
+save_root = os.path.join('checkpoints', opt.name)
 if not os.path.isdir(save_root):
     os.makedirs(save_root)
 
 # get the data
-climate_data = ClimateDataset(opt=args)
+climate_data = ClimateDataset(opt=opt)
 climate_data_loader = DataLoader(climate_data,
-                                 batch_size=args.batch_size,
-                                 shuffle=not args.no_shuffle,
-                                 num_workers=int(args.n_threads))
+                                 batch_size=opt.batch_size,
+                                 shuffle=not opt.no_shuffle,
+                                 num_workers=int(opt.n_threads))
 
 # load the model
-edgan_model = Edgan(opt=args)
-optimizer = torch.optim.Adam(edgan_model.parameters(), lr=args.lr)  # TODO which optimizer / lr / lr decay
+edgan_model = Edgan(opt=opt)
+optimizer = torch.optim.Adam(edgan_model.parameters(), lr=opt.lr)  # TODO which optimizer / lr / lr decay
 
-for epoch in range(args.n_epochs):
+for epoch in range(opt.n_epochs):
     train_loss = 0
     edgan_model.train()
     for batch_idx, data in enumerate(climate_data_loader, 0):
@@ -66,7 +34,6 @@ for epoch in range(args.n_epochs):
         coarse_pr = data['coarse_pr'].to(device)
         cell_area = data['cell_area'].to(device)
         orog = data['orog'].to(device)
-
 
         optimizer.zero_grad()
         recon_x, mu, log_var = edgan_model(fine_pr=fine_pr, coarse_pr=coarse_pr, orog=orog)
@@ -77,7 +44,7 @@ for epoch in range(args.n_epochs):
         optimizer.step()
 
         # todo add visualizer class
-        if batch_idx % args.log_interval == 0:
+        if batch_idx % opt.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tBCE Loss: {:.7f}\tKL Loss: {:.7f}\tcycle Loss {:.7f}'
                   '\tLoss: {:.7f}'.format(
                     epoch, batch_idx * len(fine_pr), len(climate_data_loader.dataset),
@@ -87,8 +54,8 @@ for epoch in range(args.n_epochs):
                     cycle_loss.item() / len(fine_pr),
                     loss.item() / len(fine_pr)))
         # todo make logging cluster ready
-        if batch_idx % args.plot_interval == 0:
-            vmin = args.threshold
+        if batch_idx % opt.plot_interval == 0:
+            vmin = opt.threshold
             vmax = 2
             plt.imshow(fine_pr[0].view(8, 8).detach().numpy(), vmin=vmin, vmax=vmax)
             plt.show()
@@ -96,12 +63,12 @@ for epoch in range(args.n_epochs):
             plt.show()
             # todo make plotting cluster ready
 
-    if epoch % args.save_interval == 0:
+    if epoch % opt.save_interval == 0:
         save_name = "epoch_{}.pth".format(epoch)
         save_dir = os.path.join(save_root, save_name)
-        if len(args.gpu_ids) > 0 and torch.cuda.is_available():
+        if len(opt.gpu_ids) > 0 and torch.cuda.is_available():
             torch.save(edgan_model.module.cpu().state_dict(), save_dir)
-            edgan_model.cuda(args.gpu_ids[0])
+            edgan_model.cuda(opt.gpu_ids[0])
         else:
             torch.save(edgan_model.cpu().state_dict(), save_dir)
     print('====> Epoch: {} Average loss: {:.4f}'.format(
