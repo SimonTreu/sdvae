ff058b9d20fbd54252121f9dcee81a0de2bd746d
Simon Treu <sitreu@pik-potsdam.de>
2018-08-30 17:43:53 +0200

    added convolutional generator

--- models/edgan.py
+++ models/edgan.py
@@ -24,10 +24,16 @@ class Edgan(nn.Module):
         self.mu = nn.Sequential(fc_layer_1, relu_1, mu)
         self.log_var = nn.Sequential(fc_layer_1, relu_1, log_var)
 
-        # decoder TODO better generator
-        self.decode = nn.Sequential(nn.Linear(self.nz+self.no+1, hidden_layer_size),
+        decoder_input_size = self.nz+self.no+1
+        self.decode = nn.Sequential(nn.ConvTranspose2d(in_channels=decoder_input_size,
+                                                       out_channels=decoder_input_size * 8,
+                                                       kernel_size=4, stride=1, padding=0),
+                                    nn.BatchNorm2d(decoder_input_size * 8),
                                     nn.ReLU(),
-                                    nn.Linear(hidden_layer_size, self.input_size),
+                                    nn.ConvTranspose2d(in_channels=decoder_input_size*8,
+                                                       out_channels=1, kernel_size=4,
+                                                       stride=2, padding=1),
+                                    nn.BatchNorm2d(1),
                                     nn.Threshold(value=threshold, threshold=threshold)
                                     )
         if self.no > 0:
@@ -49,7 +55,7 @@ class Edgan(nn.Module):
         if self.no > 0:
             orog.unsqueeze_(1)  # bring into shape (N, n_ch, W, H)
             o = self.encode_orog(orog)
-            return self.decode(torch.cat((z, coarse_pr, o.view(-1,self.no)), 1)), mu, log_var
+            return self.decode(torch.cat((z, coarse_pr, o.view(-1,self.no)), 1).unsqueeze(-1).unsqueeze(-1)), mu, log_var
         else:
             return self.decode(torch.cat((z, coarse_pr), 1)), mu, log_var
 
@@ -65,7 +71,7 @@ class Edgan(nn.Module):
     # Reconstruction + KL divergence losses summed over all elements and batch
     def loss_function(self, recon_x, x, mu, log_var, coarse_pr, cell_area):
         # todo change name of BCE
-        BCE = nn.functional.mse_loss(recon_x, x.view(-1, self.input_size), size_average=False)
+        BCE = nn.functional.mse_loss(recon_x, x.unsqueeze(1), size_average=False)
 
         # see Appendix B from VAE paper:
         # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
@@ -74,7 +80,7 @@ class Edgan(nn.Module):
         KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
 
         # cycle loss as mean squared error
-        recon_average = get_average(recon_x, cell_area.contiguous().view(-1, self.input_size))
+        recon_average = get_average(recon_x.view(-1,64), cell_area.contiguous().view(-1, self.input_size))
         cycle_loss = torch.mean(coarse_pr.view(-1).sub(recon_average).pow(2)) * self.lambda_cycle_l1
         return BCE, KLD, cycle_loss, BCE + KLD + cycle_loss
 
