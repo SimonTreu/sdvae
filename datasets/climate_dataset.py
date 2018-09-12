93b4179c1e8a40bd860eca5704651d3e02beadd3
Simon Treu <sitreu@pik-potsdam.de>
2018-09-12 14:07:55 +0200

    Added Surrounding course Precipitation do dataset

--- datasets/climate_dataset.py
+++ datasets/climate_dataset.py
@@ -30,26 +30,94 @@ class ClimateDataset(Dataset):
         # random crop
         w = sample.shape[-2]
         h = sample.shape[-1]
-        w_offset = random.randint(0, max(0, w - self.fine_size-1))
-        h_offset = random.randint(0, max(0, h - self.fine_size-1))
+        w_offset = random.randint(self.fine_size, max(0, w - 2 * self.fine_size - 1))
+        h_offset = random.randint(self.fine_size, max(0, h - 2 * self.fine_size - 1))
+
+        # todo Normalize wind
+        # normalize pr
+        sample[0].sub_(self.norm_parameters['mean_pr']).div_(self.norm_parameters['std_pr'])
+        # normalize orog
+        sample[-2].sub_(self.norm_parameters['mean_orog']).div_(self.norm_parameters['std_orog'])
 
         input_sample = sample[:, h_offset:h_offset+self.fine_size, w_offset:w_offset+self.fine_size]
-        # get precipitation
+
+        sample_ul = sample[:,
+                    h_offset+self.fine_size:h_offset+2*self.fine_size,
+                    w_offset-self.fine_size:w_offset]
+        sample_u = sample[:,
+                   h_offset+self.fine_size:h_offset + 2 * self.fine_size,
+                   w_offset:w_offset+self.fine_size]
+        sample_ur = sample[:,
+                    h_offset+self.fine_size:h_offset + 2 * self.fine_size,
+                    w_offset+self.fine_size:w_offset+2*self.fine_size]
+
+        sample_l = sample[:,
+                   h_offset:h_offset+self.fine_size,
+                   w_offset-self.fine_size:w_offset]
+        sample_r = sample[:,
+                   h_offset:h_offset+self.fine_size,
+                   w_offset+self.fine_size:w_offset+2*self.fine_size]
+
+        sample_bl = sample[:,
+                    h_offset-self.fine_size:h_offset,
+                    w_offset-self.fine_size:w_offset]
+        sample_b = sample[:,
+                   h_offset-self.fine_size:h_offset,
+                   w_offset:w_offset+self.fine_size]
+        sample_br = sample[:,
+                    h_offset - self.fine_size:h_offset,
+                    w_offset + self.fine_size:w_offset + 2 * self.fine_size]
+
         cell_area = input_sample[-1]
         orog = input_sample[-2]
-        # todo add wind
         fine_pr = input_sample[0]
+        pr_ul = sample_ul[0]
+        pr_u = sample_u[0]
+        pr_ur = sample_ur[0]
+
+        pr_l = sample_l[0]
+        pr_r = sample_r[0]
+
+        pr_bl = sample_bl[0]
+        pr_b = sample_b[0]
+        pr_br = sample_br[0]
+
         fine_uas = input_sample[1]
         fine_vas = input_sample[2]
 
-        # normalize parameters
-        # todo Normalize wind
-        fine_pr.sub_(self.norm_parameters['mean_pr']).div_(self.norm_parameters['std_pr'])
-        orog.sub_(self.norm_parameters['mean_orog']).div_(self.norm_parameters['std_orog'])
 
         coarse_pr = get_average(fine_pr.contiguous().view(1, -1),
                                 cell_area=cell_area.contiguous().view(1, -1)
                                 ).unsqueeze(0).unsqueeze(0)  # Shape [C=1, W=1, H=1]
+
+        coarse_ul = get_average(pr_ul.contiguous().view(1, -1),
+                                cell_area=cell_area.contiguous().view(1, -1)
+                                ).unsqueeze(0).unsqueeze(0)
+        coarse_u = get_average(pr_u.contiguous().view(1, -1),
+                                cell_area=cell_area.contiguous().view(1, -1)
+                                ).unsqueeze(0).unsqueeze(0)
+        coarse_ur = get_average(pr_ur.contiguous().view(1, -1),
+                                cell_area=cell_area.contiguous().view(1, -1)
+                                ).unsqueeze(0).unsqueeze(0)
+
+        coarse_l = get_average(pr_l.contiguous().view(1, -1),
+                                cell_area=cell_area.contiguous().view(1, -1)
+                                ).unsqueeze(0).unsqueeze(0)
+        coarse_r = get_average(pr_r.contiguous().view(1, -1),
+                                cell_area=cell_area.contiguous().view(1, -1)
+                                ).unsqueeze(0).unsqueeze(0)
+
+        coarse_bl = get_average(pr_bl.contiguous().view(1, -1),
+                                cell_area=cell_area.contiguous().view(1, -1)
+                                ).unsqueeze(0).unsqueeze(0)
+        coarse_b = get_average(pr_b.contiguous().view(1, -1),
+                                cell_area=cell_area.contiguous().view(1, -1)
+                                ).unsqueeze(0).unsqueeze(0)
+        coarse_br = get_average(pr_br.contiguous().view(1, -1),
+                                cell_area=cell_area.contiguous().view(1, -1)
+                                ).unsqueeze(0).unsqueeze(0)
+
+
         uas = get_average(fine_uas.contiguous().view(1, -1),
                           cell_area=cell_area.contiguous().view(1, -1)
                           ).unsqueeze(0).unsqueeze(0)
@@ -64,7 +132,10 @@ class ClimateDataset(Dataset):
         orog.unsqueeze_(0)
 
         return {'fine_pr': fine_pr, 'file_path': sample_path, 'coarse_pr': coarse_pr,
-                'cell_area': cell_area, 'orog': orog, 'uas': uas, 'vas': vas, 'time': end_time}
+                'cell_area': cell_area, 'orog': orog, 'uas': uas, 'vas': vas, 'time': end_time,
+                'coarse_ul': coarse_ul, 'coarse_u': coarse_u, 'coarse_ur': coarse_ur,
+                'coarse_l': coarse_l, 'coarse_r': coarse_r,
+                'coarse_bl': coarse_bl, 'coarse_b': coarse_b, 'coarse_br': coarse_br, }
 
 
 # Helper Methods
