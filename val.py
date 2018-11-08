2efe899f26c893758caa5ab1ea52ea2f47f8fad3
Simon Treu <sitreu@pik-potsdam.de>
2018-11-08 11:55:56 +0100

    refactoring

--- val.py
+++ val.py
@@ -15,12 +15,10 @@ def main():
     n_samples = opt.n_samples
     upscaler = Upscale(size=opt.fine_size+2*opt.scale_factor, scale_factor=opt.scale_factor)
     device = torch.device("cuda" if len(opt.gpu_ids) > 0 else "cpu")
-    save_root = os.path.join('checkpoints', opt.name)
-    if not os.path.isdir(save_root):
-        os.makedirs(save_root)
+    load_root = os.path.join('checkpoints', opt.name)
     load_epoch = opt.load_epoch if opt.load_epoch >= 0 else 'latest'
-    save_name = "epoch_{}.pth".format(load_epoch)
-    save_dir = os.path.join(save_root, save_name)
+    load_name = "epoch_{}.pth".format(load_epoch)
+    load_dir = os.path.join(load_root, load_name)
     outdir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, load_epoch))
     if not os.path.exists(outdir):
         os.makedirs(outdir)
@@ -30,7 +28,7 @@ def main():
 
     # load the model
     edgan_model = Edgan(opt=opt, device=device).to(device)
-    edgan_model.load_state_dict(torch.load(save_dir))
+    edgan_model.load_state_dict(torch.load(load_dir))
 
     # Iterate val cells and compute #n_samples reconstructions.
     # todo seperate val and test
@@ -140,46 +138,6 @@ def main():
             output_dataset.close()
             index += 1
         input_dataset.close()
-
-    '''
-    # Plot Figure to evaluate latent space
-    viz = ValidationViz(opt)
-    viz.plot_latent_walker(edgan_model, climate_data)
-    
-    base_path = os.path.join(save_root, 'epoch_{}_all'.format(opt.load_epoch))
-    if not os.path.isfile(os.path.abspath(base_path + '_coarse_pr.pt')):
-        all_fine_pr = None
-        all_recon_pr = None
-        all_coarse = None
-        for batch_idx, data in enumerate(climate_data_loader, 0):
-            fine_pr = data['fine_pr'].to(device)
-            coarse_pr = data['coarse_pr'].to(device)
-            cell_area = data['cell_area'].to(device)
-            orog = data['orog'].to(device)
-            recon_pr = edgan_model.get_picture(coarse_precipitation=coarse_pr,
-                                               coarse_ul=data['coarse_ul'].to(device),
-                                               coarse_u=data['coarse_u'].to(device),
-                                               coarse_ur=data['coarse_ur'].to(device),
-                                               coarse_l=data['coarse_l'].to(device),
-                                               coarse_r=data['coarse_r'].to(device),
-                                               coarse_bl=data['coarse_bl'].to(device),
-                                               coarse_b=data['coarse_b'].to(device),
-                                               coarse_br=data['coarse_br'].to(device),
-                                               orog=orog)
-            if not(all_fine_pr is None):
-                all_fine_pr = torch.cat((all_fine_pr, fine_pr), 0)
-                all_recon_pr = torch.cat((all_recon_pr, recon_pr), 0)
-                all_coarse = torch.cat((all_coarse, coarse_pr), 0)
-            else:
-                all_fine_pr = fine_pr
-                all_recon_pr = recon_pr
-                all_coarse = coarse_pr
-        save_val_data(all_fine_pr, all_coarse, all_recon_pr, base_path)
-    
-    val_obj = ValObj(base_path, min=20, max=50)
-    val_obj.evaluate_distribution()
-    
-    '''
 
 
 if __name__=='__main__':
